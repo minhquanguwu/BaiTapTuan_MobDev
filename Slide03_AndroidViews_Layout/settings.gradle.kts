eb9ef98568e56ca3872fdb5f045e8c9697f0f3eb
quangpm <quangpm309@gmail.com>
2023-09-06 22:00:51 +0700

    Slide03

---
+++ Slide03_AndroidViews_Layout/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Slide03_AndroidViews_Layout"
+include(":app")
+ 
