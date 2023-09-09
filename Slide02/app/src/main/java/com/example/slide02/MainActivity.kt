f80d11fc5c0c0fbab0eb64ea97cbaac2f97ecde6
quangpm <quangpm309@gmail.com>
2023-09-10 00:47:26 +0700

    Update Slide 02_2

--- Slide02/app/src/main/java/com/example/slide02/MainActivity.kt
+++ Slide02/app/src/main/java/com/example/slide02/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.slide02
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuInflater
 import android.widget.NumberPicker
 import android.widget.TextView
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,7 +19,15 @@ class MainActivity : AppCompatActivity() {
         numberPicker.minValue = 900
         numberPicker.maxValue = 2000
         numberPicker.value = 999
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_demo, menu)
+        return true
+    }
+
 
 
 }
