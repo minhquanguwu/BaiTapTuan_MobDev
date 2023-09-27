765ab9fc06c6f9e608f454db5195255580791c75
quangpm <quangpm309@gmail.com>
2023-09-27 23:28:31 +0700

    Slide 08

---
+++ Slide08/app/src/main/java/com/example/slide08/MyBroadcastReceiver.kt
@@ -0,0 +1,11 @@
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class MyBroadcastReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        Toast.makeText(context, "Intent receive successfully!", Toast.LENGTH_SHORT).show()
+
+    }
+}
